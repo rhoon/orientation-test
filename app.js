7386de6c347e4818eadee0bd0859b3c673d73086
Riley Hoonan <hoonr188@newschool.edu>
2016-08-26 19:43:53 +0000

    Now with Aaron's Node Code

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-Hello World
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Nodes on nodes in nodes through nodes.\n');
+}).listen(process.env.PORT, process.env.IP);
